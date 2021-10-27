7b6856734a747bd3592c79fba9dfd23b44298c07
David Acum <davidacum11@gmail.com>
2021-10-27 14:08:07 +0100

    Added Sales

---
+++ Data/Model/Sales.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpProj.Data.Model
+{
+    class Sales
+    {
+        public int SaleID { get; set; }
+        public string ProductName { get; set; }
+        public int Quantity { get; set; }
+        public decimal Price { get; set; }
+        public DateTime SaleDate { get; set; }
+
+        public override string ToString()
+        {
+            return $"Sales info: Sale ID: {SaleID}, ProductID: {ProductName}, Quantity: {Quantity}, Product Cost: {Price}, Sale Date: {SaleDate}";
+        }
+
+
+    }
+}
